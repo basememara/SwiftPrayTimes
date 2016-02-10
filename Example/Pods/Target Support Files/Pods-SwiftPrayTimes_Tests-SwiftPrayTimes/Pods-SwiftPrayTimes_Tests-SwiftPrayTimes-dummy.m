9bb7f3ef9cbc56b3b0cef0c77606e7d9f1ecbf4d
Basem Emara <basememara@gmail.com>
2016-02-10 18:36:34 -0500

    Release 1.1.4.

--- Example/Pods/Target Support Files/Pods-SwiftPrayTimes_Tests-SwiftPrayTimes/Pods-SwiftPrayTimes_Tests-SwiftPrayTimes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftPrayTimes_Tests_SwiftPrayTimes : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftPrayTimes_Tests_SwiftPrayTimes
-@end
